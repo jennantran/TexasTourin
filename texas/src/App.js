588d1466f5c73189fae6a687467ba99a0f14347b
Jennifer Tran <jennantran@gmail.com>
2020-11-15 16:05:00 -0800

    switch index and app code

--- texas/src/App.js
+++ texas/src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import HomePage from './HomePage/HomePage';
+import Nav from './Nav/Nav';
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+class  App extends Component {
+  constructor(){
+    return (
+      <div className='App'>
+          <nav><Nav/></nav>
+          <Route exact path='/' component={HomePage}/>
+      </div>
+    );
+  }
 }
-
 export default App;
