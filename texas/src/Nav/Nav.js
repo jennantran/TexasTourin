084f2ace8f69f80d402e4a8603138421d21623b0
Jennifer Tran <jennantran@gmail.com>
2020-11-20 13:27:55 -0800

    remove menu item and add nav bar icon page

--- texas/src/Nav/Nav.js
+++ texas/src/Nav/Nav.js
@@ -2,36 +2,18 @@ import React, { Component } from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.css';
-import Dropdown from '../Dropdown/Dropdown';
-
 
 function Nav(){
     const [click, setClick] = useState(false);
-    const [dropdown, setDropdown] = useState(false);
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
 
-    const onMouseEnter = () => {
-        if(window.innerWidth < 960){
-            setDropdown(false);
-        }else{
-            setDropdown(true);
-        }
-    }
-    const onMouseLeave = () => {
-        if(window.innerWidth < 960){
-            setDropdown(false);
-        }else{
-            setDropdown(false);
-        }
-    }
 
         return (
             <nav className='nav'>
                 <Link to='/' className='title'>
                         <h2><span>T</span><span>T</span></h2>
                 </Link>
-                <Dropdown></Dropdown>
                 <div className='menu-icon' onClick={handleClick}>
                     <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                 </div>
