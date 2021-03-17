f5136147cf379c625953521cdd07a1db9148e996
Jennifer Tran <jennantran@gmail.com>
2021-03-17 16:24:02 -0700

    add our story files

---
+++ texas/src/OurStory/OurStory.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './OurStory.css';
+
+class OurStory extends Component {
+    
+    render(){
+        return (
+            <div>
+                <section className='OurStory'>
+              
+                </section>
+            </div>
+        );
+
+    }
+
+}
+
+export default 'OurStory';
