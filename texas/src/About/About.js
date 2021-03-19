54bb25f83eab14c6ed2dd041fcc768394c9a3eea
Jennifer Tran <jennantran@gmail.com>
2021-03-18 18:28:22 -0700

    add about

---
+++ texas/src/About/About.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import './About.css';
+
+
+class About extends Component {
+    render(){
+        return (
+            <div>
+                <section className='teamInfo'>
+                    <p className='story'> We, Ira Mascardo, Jennifer Tran, and Rafael Loredo, are a group of coding enthusiasts. We all met as coworkers in Plano, Texas at a reputable tech company. Every week,
+                        we often met up and explore the city. With the city's limitness offerings of restaurants, and activities,
+                        we found it hard to find online resources to quickly find the city hotspots. TexasTourin came to fruition to help
+                        fellow adventurers navigate the city and fall in love with the LoneStar state. 
+                    </p>
+                </section>
+            </div>
+        );
+
+    }
+
+}
+
+export default 'About';
